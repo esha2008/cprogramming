8150623d7fdecde7526188699ec11fc70548ebc6
Piyush Srivastava <piyush.0101@gmail.com>
2014-12-28 15:09:33 -0600

    Adding a stack.c program

---
+++ stack.c
@@ -0,0 +1,5 @@
+/* Write a program to implement a Stack using Linked List
+ * Stack should support the following operations
+ * void push(int number): Pushes an element on top of the stack
+ * int pop(): Removes the last pushed element from the stack
+ */
