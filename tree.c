3be72b37b8906651b89713624b7274d27c567948
Piyush Srivastava <piyush.0101@gmail.com>
2015-01-04 11:27:55 -0600

    Adding tree traversals

--- tree.c
+++ tree.c
@@ -26,17 +26,45 @@ struct Node * bst_add(int number, struct Node * current) {
     return current;
 }
 
+struct Node * add(int number, struct Node * current) {
+    if (current == NULL) {
+        struct Node *new_node = (struct Node *) malloc(sizeof(struct Node));
+        new_node -> element = number;
+        new_node -> right = NULL;
+        new_node -> left = NULL;
+        return new_node;
+    }
+    if (current -> left == NULL) {
+        current -> left = add(number, current -> left);
+    }
+    else {
+        current -> right = add(number, current -> right);
+    }
+    return current;
+}
+
 void in_order(struct Node *root) {
     if (root -> left != NULL) in_order(root -> left);
     printf("%d\n", root -> element);
     if (root -> right != NULL) in_order(root -> right);
 }
 
+void preorder(struct Node *root){
+    printf("%d \n",root->element);
+    if(root->left !=NULL)preorder(root->left);
+    if(root->right !=NULL)preorder(root->right);
+}
+void postorder(struct Node *root)
+{
+    if(root->left !=NULL)postorder(root->left);
+    if(root->right!=NULL) postorder(root->right);
+    printf("%d\n",root->element);
+}
 int main() {
     root = bst_add(4, root);
     root = bst_add(5, root);
-    root = bst_add(3, root);
     root = bst_add(1, root);
-    root = bst_add(7, root);
-    in_order(root);
+    //root = add(2, root);
+    //root = add(7, root);
+    postorder(root);
 }
